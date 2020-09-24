9b32b026889e71970ded6758c38da17fdafd4930
Ayden Elliott <dambuger@gmail.com>
2020-09-24 08:36:09 -0600

    Fix gitgnore

---
+++ Project 1/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 20;
+    public float turnSpeed;
+    public float gravity = 20;
+    
+    public float horizontalInput;
+    public float verticalInput;
+    public float jumpInput;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    
+    horizontalInput = Input.GetAxis("Horizontal");
+    verticalInput = Input.GetAxis("Vertical");
+    jumpInput = Input.GetAxis("Jump");
+    
+        // rotates the truck
+         transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
+         
+        // Moves the truck forward
+         transform.Translate(Vector3.forward * Time.deltaTime * speed * verticalInput);
+         
+         // Makes the truck jump
+        transform.Translate(Vector3.up * Time.deltaTime * gravity * jumpInput);
+        
+    }
+}
